bb6754d0fb654d5224c3f6e51a4f5c0084ca5c2c
Javier Rivera <ingjrz@gmail.com>
2023-07-18 09:50:47 -0600

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreWebAPI.Models
+{
+    public class Product
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; } = "Default Name";
+        public string? Description { get; set; }
+        public string? Price { get; set; }
+        public string? Image { get; set;}
+
+    }
+}
