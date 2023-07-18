23ce7c8577c78610e647f92f12099e6c99801cb1
Javier Rivera <ingjrz@gmail.com>
2023-07-18 10:02:02 -0600

    updating gitignore

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,12 +1,11 @@
 ﻿namespace CoreWebAPI.Models
 {
     public class Product
-    {   
+    {
         public int Id { get; set; }
         public string Name { get; set; } = "Default Name";
         public string? Description { get; set; }
         public string? Price { get; set; }
-        public string? Image { get; set;}
-
+        public string? Image { get; set; }
     }
 }
